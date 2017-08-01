24cdf7211994dfd9e81ab517326f91f3b9e849c9
Nicola Marcacci Rossi <nicolamr@gmail.com>
2017-08-01 12:35:11 +0200

    better dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -17,17 +17,35 @@ const DashboardComponent = ({user}) => (
         <p>Thank you for joining sorters club! This is your dashboard.</p>
         <p>As the platform develops, you will find more features here.</p>
         <p>If there is a feature you wish you can discuss this with us on <a href="https://www.patreon.com/nickredmark" target="_blank">Patreon</a>.</p>
-        <ul>
-            {user.local && user.local.username &&
-                <li>
-                    <a href={`/u/${user.local.username}`}>See your profile</a>
-                </li>
-            }
-            <li><a href="/profile">Manage your profile</a></li>
-            <li><a href="/account">Manage your account</a></li>
-            <li><a href="/reads">Manage your reading list</a></li>
-            <li><a href="/users">Find other sorters</a></li>
-        </ul>
+          <div className="row">
+            <div className="col-md-4">
+              <h2>Profile</h2>
+              <p>Edit your public profile that can be seen by others.</p>
+              {user.local && user.local.username &&
+                <p>
+                    Your profile can be found at <a href={`/u/${user.local.username}`}>{`/u/${user.local.username}`}</a>.
+                </p>
+              }
+              <p><a className="btn btn-default" href="/profile" role="button">Manage profile</a></p>
+            </div>
+            <div className="col-md-4">
+              <h2>Account</h2>
+              <p>Set your username, password etc.</p>
+              <p><a className="btn btn-default" href="/account" role="button">Manage account</a></p>
+            </div>
+            <div className="col-md-4">
+              <h2>Reading list</h2>
+              <p>Create a list of things you read and intend to read, with links to your articles and videos.</p>
+              <p><a className="btn btn-default" href="/reads" role="button">Manage reading list</a></p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <h2>Sorters</h2>
+              <p>See what other sorters are doing.</p>
+              <p><a className="btn btn-default" href="/users" role="button">Sorters</a></p>
+            </div>
+          </div>        
     </div>
 )
 const Dashboard = compose(
